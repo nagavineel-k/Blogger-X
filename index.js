aa0b6de649aa90d9ca92c51e5f4c12df7336a7f4
nagavineel96 <nagavineel96@gmail.com>
2021-03-26 00:41:40 +0530

    Created user register schema and configured with validators in nodejs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//------------------ Database Connection --------------------------------->
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
     if (err) {
@@ -14,7 +19,12 @@ mongoose.connect(config.uri, (err) => {
     }
 });
 
+//------------------ Parse application --------------------------------------->
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//------------------ Provide Static Directory for Frontend --------------------------------->
 app.use(express.static(__dirname + '/client/dist/client/'));
+app.use('/authentication', authentication);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
