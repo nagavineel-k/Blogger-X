0af689066924790ddc2407d7f97b1e010f266c0e
nagavineel96 <nagavineel96@gmail.com>
2021-03-26 22:42:08 +0530

    Added Registration form using reactive forms with validations

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: HomeComponent }
 ];
 
